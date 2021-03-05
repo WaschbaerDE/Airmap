e738f961d51624e0bf67c5b6ae08203d015a18e3
WaschbaerDE <44826829+WaschbaerDE@users.noreply.github.com>
2021-03-05 17:17:31 +0100

    Init Commit of JavaFX Gui from SceneBuilder

---
+++ src/com/ebbrechtair/Main.java
@@ -0,0 +1,26 @@
+package com.ebbrechtair;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("ressources/ebbrechtAirwayGui.fxml"));
+            primaryStage.setTitle("Ebbrecht-Air");
+            primaryStage.setScene(new Scene(root, 1243, 1000));
+            primaryStage.show();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
