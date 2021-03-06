e726cc745d7cb895dce002aa97bc6951f9a9be55
WaschbaerDE <44826829+WaschbaerDE@users.noreply.github.com>
2021-03-06 01:34:27 +0100

    Changes to: -Map: Added getter for leftupper and rightbottomcorner coordinates in km

--- src/com/ebbrechtair/javafx/Map.java
+++ src/com/ebbrechtair/javafx/Map.java
@@ -1,29 +1,25 @@
 package com.ebbrechtair.javafx;
 
-import com.ebbrechtair.classes.GeoCoordinate;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-import java.beans.EventHandler;
-
 public class Map extends Canvas {
-    private double[] middlepoint;//This is the 500|500 point / the middlepoint corner of the map/canvas in km
-    private double zoomFaktor;//This is the faktor between pixel to distance in km 1000px = 100km BASIC // VERDOPPELT SICH ALLE 4 RAD KLICKS
+    private final double[] middlepoint;//This is the 500|500 point / the middlepoint corner of the map/canvas in km
+    private double zoomFaktor;//This is the faktor between pixel to distance in km 1000px = 100km BASIC 
+                                // VERDOPPELT SICH ALLE 4 RAD KLICKS
     private double lastX;
     private double lastY;
 
 
-    private GraphicsContext context = this.getGraphicsContext2D();
+    private final GraphicsContext context = this.getGraphicsContext2D();
 
     public Map() {
         super(1000, 1000);
         this.zoomFaktor = 0.1;
         this.middlepoint = new double[]{0,0};
 
-
         drawGrit();
-        //drawTestAirways();
 
         //This event get triggered on Scrolling with Mouse3-button
         //the new zoomfaktor is the old one times the scroll /40 * the faktor to zoom x2 with 4 wheel clicks
@@ -46,14 +42,20 @@ public class Map extends Canvas {
             this.middlepoint[1] = middlepoint[1]+(this.lastY-event.getY())*zoomFaktor;
             this.lastX = event.getX();
             this.lastY = event.getY();
-
-            System.out.println(this.middlepoint[0]+" "+this.middlepoint[1]);
         });
-
     }
 
+    //gets coords of leftuppercorner in km
+    public double[] getLeftUpperCorner() {
+        return new double[]{(this.middlepoint[0]-500)*zoomFaktor,(this.middlepoint[1]+500)*zoomFaktor};
+    }
 
+    //gets coords of rigthbottomcorner in km
+    public double[] getRigthBottomCorner() {
+        return new double[]{(this.middlepoint[0]+500)*zoomFaktor,(this.middlepoint[1]-500)*zoomFaktor};
+    }
 
+    //draws basic grid on map
     private void drawGrit(){
         for(int i = 0; i < 10000; i = i+100) {
             drawGritLines(i, 0, i, 9000);
@@ -71,33 +73,4 @@ public class Map extends Canvas {
         context.strokeLine(x1, y1, x2, y2);
     }
 
-
-
-//    public void drawTestAirways(){
-//        for (int i = 1; i < 30; i++) {
-//            lineCoordinates[0] = (i*10);
-//            lineCoordinates[1] = (i*20);
-//            lineCoordinates[2] = (i*30);
-//            lineCoordinates[3] = (i*40);
-//            drawAirwayLines(lineCoordinates[0],lineCoordinates[1],lineCoordinates[2],lineCoordinates[3]);
-//        }
-//        lineCoordinates[0] = (30*10);
-//        lineCoordinates[1] = (30*20);
-//        lineCoordinates[2] = (30*30);
-//        lineCoordinates[3] = (30*40);
-////        colorAirwayRoutes(lineCoordinates[0],lineCoordinates[1],lineCoordinates[2],lineCoordinates[3]);
-//    }
-
-//    public void drawAirwayLines(int x1, int y1, int x2, int y2) {
-//        context.setLineWidth(2);
-//        context.setStroke(Color.GREEN);
-//        context.strokeLine(x1, y1, x2, y2);
-//    }
-
-//    public void colorAirwayRoutes(int x1, int y1, int x2, int y2) {
-//        context.setLineWidth(5);
-//        context.setStroke(Color.MAGENTA);
-//        context.strokeLine(x1, y1, x2, y2);
-//    }
 }
-
